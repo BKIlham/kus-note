eb2820eb7eeba7bafe89ab344f09605e35a1d808
BintangKus <bintangkus308@gmail.com>
2025-08-18 17:16:09 +0700

    install mongoose@7.0.3

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,3 +8,4 @@ app.use("/api/notes", notesRoutes);
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001")
 });
+//mongodb+srv://bintangkus308:ayX3WfKdpY68cSGh@cluster0.otu0uqp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
